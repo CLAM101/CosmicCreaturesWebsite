62dd987dcdb26a1bca8af879a5a33b7e3a5a3b50
MattDJ <mattdj@honcho.agency>
2024-01-09 08:37:38 +0000

    003: Add purple variations

--- tailwind_colours.js
+++ tailwind_colours.js
@@ -1,6 +1,10 @@
 const colors = (global.colors = {
   purple: {
-    DEFAULT: "#4647c5"
+    300: "#8f90da",
+    400: "#6b6ccf",
+    DEFAULT: "#4647c5",
+    600: "#2e2faa",
+    700: "#1f2092",
   },
   black: {
     DEFAULT: "#01091E",
